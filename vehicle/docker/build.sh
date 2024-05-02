30a0d621e879e0843e42a4220bdce026715d4f96
idcnvrsn <>
2024-05-03 01:22:07 +0900

    Dockerfileリファクタリング

--- vehicle/docker/build.sh
+++ vehicle/docker/build.sh
@@ -1,6 +1,8 @@
 BASENAME=vehicle_ros2
+ROS_DISTRO=humble
 
 docker buildx build \
+  --build-arg ROS_DISTRO=${ROS_DISTRO} \
   --build-arg HTTP_PROXY=${HTTP_PROXY} \
   --build-arg HTTPS_PROXY=${HTTPS_PROXY} \
   --build-arg NO_PROXY=${NO_PROXY} \
