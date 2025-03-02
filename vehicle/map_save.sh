5c86204e498d3dca82a49f88493c63803deaf76f
idcnvrsn <>
2025-03-02 12:49:20 +0900

    map保存のためのスクリプト追加

---
+++ vehicle/map_save.sh
@@ -0,0 +1,11 @@
+sudo cp my_room.launch.py /opt/ros/humble/share/turtlebot3_gazebo/launch/
+sudo cp worlds/my_room.world /opt/ros/humble/share/turtlebot3_gazebo/worlds
+terminator -e "ros2 launch turtlebot3_gazebo my_room.launch.py" &
+
+terminator -e "ros2 launch slam_toolbox online_async_launch.py params_file:=slam_toolbox_params.yaml" &
+terminator -e "ros2 run rviz2 rviz2 -d $(ros2 pkg prefix nav2_bringup)/share/nav2_bringup/rviz/nav2_default_view.rviz" &
+terminator -e "ros2 run turtlebot3_teleop teleop_keyboard" &
+
+# map保存は以下を実施する。 上書き保存はできない模様
+# ros2 run nav2_map_server map_saver_cli -f my_map
+
