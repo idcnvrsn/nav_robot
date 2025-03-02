dd975dbcc06e956816381afae7f0ed7b7dcf889a
idcnvrsn <>
2025-03-02 20:10:06 +0900

    view_sdf_rvizのインストール先を変えた

--- vehicle/docker/entrypoint.sh
+++ vehicle/docker/entrypoint.sh
@@ -6,5 +6,6 @@ echo "source /usr/share/gazebo/setup.sh" >> ~/.bashrc
 echo "export TURTLEBOT3_MODEL=burger" >> ~/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
 echo "export _colcon_cd_root=/opt/ros/humble/" >> ~/.bashrc
+echo source /opt/ros2_ws/install/setup.bash >> ~/.bashrc
 
 exec "$@"
