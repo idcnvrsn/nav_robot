dc3615b8615f58ad2cff4f80207253fc9171ad76
idcnvrsn <>
2025-03-02 21:57:42 +0900

    realsenseをgazeboで動かすためのインストールを/optに移した

--- vehicle/docker/entrypoint.sh
+++ vehicle/docker/entrypoint.sh
@@ -7,5 +7,6 @@ echo "export TURTLEBOT3_MODEL=burger" >> ~/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
 echo "export _colcon_cd_root=/opt/ros/humble/" >> ~/.bashrc
 echo source /opt/ros2_ws/install/setup.bash >> ~/.bashrc
+echo 'export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/turtlebot3_ws/src/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models' >> ~/.bashrc
 
 exec "$@"
